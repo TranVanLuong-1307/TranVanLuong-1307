536ac603aa2558e5d0f3a4e023c80fa8ee4060a4
unknown <minw62667@gmail.com>
2026-03-11 14:44:02 +0700

    lap02 đã hoàn thành các trang web của 5 thuật toán cổ điển

--- lap02/api.py
+++ lap02/api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher 
 from cipher.vigenere import VigenereCipher
-from cipher.reilfence import RailfenceCipher
+from cipher.railfence import RailfenceCipher
+from cipher.playfair import PlayFairCipher
+from cipher.transposition import TranspositionCipher
 
 app = Flask(__name__)
 caesar_cipher = CaesarCipher()
@@ -23,7 +25,7 @@ def caesar_decrypt():
     return jsonify({'decrypted_message': decrypted_text})
 
 Vigenere_cipher = VigenereCipher()
-   
+
 @app.route("/api/vigenere/encrypt", methods=["POST"])
 def vigenere_encrypt():
         data = request.json
@@ -59,8 +61,43 @@ def railfence_decrypt():
     decrypted_text = Railfence_cipher.railfence_decrypt(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
+Playfair_cipher = PlayFairCipher()
+@app.route("/api/playfair/encrypt", methods=["POST"])
+def playfair_create_matrix():
+    data = request.json
+    key = data['key']
+    
+    playfair_matrix = Playfair_cipher.create_playfair_matrix(key)
+    return jsonify({'playfair_matrix': playfair_matrix})
+
+@app.route("/api/playfair/decrypt", methods=["POST"])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = Playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = Playfair_cipher.decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_message': decrypted_text})
+
+transposition_cipher = TranspositionCipher()
+@app.route("/api/transposition/encrypt", methods=["POST"])
+def transposition_encrypt():
+    data = request.get_json
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/transposition/decrypt", methods=["POST"])
+def transposition_decrypt():
+    data = request.get_json
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
     
- 
+
     
